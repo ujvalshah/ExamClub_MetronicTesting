c9efc8fcb15791bbde3c41764f9085dd219eb87d
Ujval <ujvalshah@gmail.com>
2019-10-30 11:32:38 +0530

    PreInstall Env Variable ConsoleLog Chnage

--- preinstall.js
+++ preinstall.js
@@ -1,4 +1,4 @@
 require('dotenv').config();
 const fs = require('fs');
 
-fs.writeFile(process.env.GOOGLE_APPLICATION_CREDENTIALS, process.env.GCLOUD_CONFIG, (err) => {if(err){return console.log(err)} console.log(process.env.GOOGLE_CONFIG); console.log('file was saved')});
+fs.writeFile(process.env.GOOGLE_APPLICATION_CREDENTIALS, process.env.GCLOUD_CONFIG, (err) => {if(err){return console.log(err)} console.log(process.env.GCLOUD_CONFIG); console.log('file was saved')});
