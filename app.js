140ec8faa24f26667ffc1730025fc8927925a730
Ujval <ujvalshah@gmail.com>
2019-11-13 03:36:39 +0530

    Https redirect

--- app.js
+++ app.js
@@ -99,6 +99,14 @@ app.use(function (req, res, next) {
 //----------------------------------------------------------------------------//
 //---------------------------------Requiring Routes---------------------------//
 //----------------------------------------------------------------------------//
+if(process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`)
+      else
+        next()
+    })
+  }
 app.use(indexRoutes);
 app.use(userRoutes);
 app.use(downloadRoutes);
