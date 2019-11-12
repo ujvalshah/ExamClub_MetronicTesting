4fd6a9d643fb10f5d9389b75f78dada98f938910
Ujval <ujvalshah@gmail.com>
2019-11-13 03:41:57 +0530

    Https redirect 2

--- app.js
+++ app.js
@@ -107,6 +107,14 @@ app.use(videoRoutes);
 app.use(apiRoute);
 app.use(batchRoute);
 
+if(process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`)
+      else
+        next()
+    })
+  }
 
 //----------------------------------------------------------------------------//
 //-----------------------------------Sockets Routes---------------------------//
